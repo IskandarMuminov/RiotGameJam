7284c82795923c35e5cfbe45be05edc226cf7a1b
ARARECREATURE <Anthony.thai@student.uts.edu.au>
2025-06-23 15:01:25 +1000

    IntialisePipe

---
+++ Assets/Scripts/TurningPipe.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningPipe : MonoBehaviour
+{
+    [SerializeField] private int Counter = 0;
+
+    public GameObject gm1;
+    public GameObject gm2;
+    public GameObject gm3;
+
+    private GameObject currentGameObject ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Counter >= 2)
+            {
+                Counter = 0;
+            }
+            else
+            {
+                Counter++;
+            }
+            if (Counter == 0)
+            {
+                gm3.SetActive(false);
+                gm1.SetActive(true);
+            }
+            if (Counter == 1)
+            {
+                gm1.SetActive(false);
+                gm2.SetActive(true);
+            }
+            if (Counter == 2)
+            {
+                gm2.SetActive(false);
+                gm3.SetActive(true);
+            }
+            
+            
+        }
+    }
+}
+
