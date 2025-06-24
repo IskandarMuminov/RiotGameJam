8258f9bea6b07d88ebf5221759dfa514c843db07
ARARECREATURE <Anthony.thai@student.uts.edu.au>
2025-06-24 11:54:17 +1000

    Waiting for Interactive

--- Assets/PipeScripts/TurningPipe.cs
+++ Assets/PipeScripts/TurningPipe.cs
@@ -21,8 +21,109 @@ public class TurningPipe : MonoBehaviour
     public GameObject[] ActivateNextPipe;
 
     public GameObject[] Water;
+    
+    [Range(0,9)]
+    public int number;
 
     // Start is called before the first frame update
+    void Update()
+    {
+
+        KeyCode keytocheck = KeyCode.Alpha0 + number;
+        if (Input.GetKeyDown(keytocheck))
+        {
+            if (Counter >= 2)
+        {
+            Counter = 0;
+        }
+        else
+        {
+            Counter++;
+        }
+        if (Counter == 0)
+        {
+            gm3.SetActive(false);
+            gm1.SetActive(true);
+            if (CorrectPipe)
+            {
+                if (gm1 == CorrectPipe)
+                {
+                    RightPipe = true;
+                    foreach (GameObject NextPipe in ActivateNextPipe)
+                    {
+                        NextPipe.GetComponent<TurningPipe>().Activated = true; 
+                        NextPipe.GetComponent<TurningPipe>().ActiviatedPipe();
+                    }
+                    
+
+                }
+                else
+                {
+                    RightPipe = false;
+                    foreach (GameObject NextPipe in ActivateNextPipe)
+                    {
+                        NextPipe.GetComponent<TurningPipe>().Activated = false;
+                    }
+                    
+                }
+            }
+                
+        }
+        if (Counter == 1)
+        {
+            gm1.SetActive(false);
+            gm2.SetActive(true);
+            if (CorrectPipe)
+            {
+                if (gm2 == CorrectPipe)
+                {
+                    RightPipe = true;
+                    foreach (GameObject NextPipe in ActivateNextPipe)
+                    {
+                        NextPipe.GetComponent<TurningPipe>().Activated = true; 
+                        NextPipe.GetComponent<TurningPipe>().ActiviatedPipe();
+                    }
+                  
+                }
+                else
+                {
+                    RightPipe = false;
+                    foreach (GameObject NextPipe in ActivateNextPipe)
+                    {
+                        NextPipe.GetComponent<TurningPipe>().Activated = false;     
+                    }
+                    
+                }
+            }
+        }
+        if (Counter == 2)
+        {
+            gm2.SetActive(false);
+            gm3.SetActive(true);
+            if (CorrectPipe)
+            {
+                if (gm3 == CorrectPipe)
+                {
+                    RightPipe = true;
+                    foreach (GameObject NextPipe in ActivateNextPipe)
+                    {
+                        NextPipe.GetComponent<TurningPipe>().Activated = true;
+                        NextPipe.GetComponent<TurningPipe>().ActiviatedPipe();
+                    }
+                }
+                else
+                {
+                    RightPipe = false;
+                    foreach (GameObject NextPipe in ActivateNextPipe)
+                    {
+                        NextPipe.GetComponent<TurningPipe>().Activated = false;
+                    }
+                }
+            }
+        }
+        }
+    }
+    /*
     public void TurnPipe()
     {
         if (Counter >= 2)
@@ -117,6 +218,7 @@ public class TurningPipe : MonoBehaviour
 
         
     }
+    */
 
     private void ActiviatedPipe()
     {
--- Assets/Rock.cs
+++ Assets/Rock.cs
@@ -8,11 +8,17 @@ public class Rock : MonoBehaviour
     public GameObject Pipe2;
     public GameObject Pipe3;
 
+    [SerializeField] private GameObject Player;
     // Update is called once per frame
     void Update()
     {
-        if (Pipe1.GetComponent<TurningPipe>().RightPipe == true || Pipe2.GetComponent<TurningPipe>().RightPipe == true || Pipe3.GetComponent<TurningPipe>().RightPipe == true)
+        if (Pipe1.GetComponent<TurningPipe>().RightPipe == true && Pipe2.GetComponent<TurningPipe>().RightPipe == true && Pipe3.GetComponent<TurningPipe>().RightPipe && true)
         {
+            if (Player.GetComponent<ResourceManager>())
+            {
+                Player.GetComponent<ResourceManager>().IncreaseWater();
+            }
+            
             Destroy(this.gameObject);
         }
     }
